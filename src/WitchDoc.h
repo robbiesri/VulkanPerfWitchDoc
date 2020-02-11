f5ff847bb4032e3af449514fbfda99f6d5d5fc30
Robert Srinivasiah <robbies@gmail.com>
2020-02-10 17:19:44 -0800

    Add clang-format definition and re-format
    It's hideous

--- src/WitchDoc.h
+++ src/WitchDoc.h
@@ -5,37 +5,45 @@
 #include <unordered_map>
 
 struct LayerBypassDispatch {
-    // instance functions, used for layer-managed query pool setup
-    PFN_vkGetPhysicalDeviceProperties getPhysicalDeviceProperties;
-    PFN_vkGetPhysicalDeviceMemoryProperties getPhysicalDeviceMemoryProperties;
+  // instance functions, used for layer-managed query pool setup
+  PFN_vkGetPhysicalDeviceProperties getPhysicalDeviceProperties;
+  PFN_vkGetPhysicalDeviceMemoryProperties getPhysicalDeviceMemoryProperties;
 };
 
 class WitchDoctor {
-public:
-    WitchDoctor();
-    ~WitchDoctor();
+ public:
+  WitchDoctor();
+  ~WitchDoctor();
 
-    VkResult PostCallCreateInstance(const VkInstanceCreateInfo* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkInstance* pInstance);
-    VkResult PostCallCreateDevice(VkPhysicalDevice physicalDevice, const VkDeviceCreateInfo * pCreateInfo, const VkAllocationCallbacks * pAllocator, VkDevice * pDevice);
-    VkResult PostCallAllocateMemory(const VkResult inResult, VkDevice device, const VkMemoryAllocateInfo* pAllocateInfo, const VkAllocationCallbacks* pAllocator, VkDeviceMemory* pMemory);
-    void PostCallFreeMemory(VkDevice device, VkDeviceMemory memory, const VkAllocationCallbacks* pAllocator);
+  VkResult PostCallCreateInstance(const VkInstanceCreateInfo* pCreateInfo,
+                                  const VkAllocationCallbacks* pAllocator,
+                                  VkInstance* pInstance);
+  VkResult PostCallCreateDevice(VkPhysicalDevice physicalDevice,
+                                const VkDeviceCreateInfo* pCreateInfo,
+                                const VkAllocationCallbacks* pAllocator,
+                                VkDevice* pDevice);
+  VkResult PostCallAllocateMemory(const VkResult inResult, VkDevice device,
+                                  const VkMemoryAllocateInfo* pAllocateInfo,
+                                  const VkAllocationCallbacks* pAllocator,
+                                  VkDeviceMemory* pMemory);
+  void PostCallFreeMemory(VkDevice device, VkDeviceMemory memory,
+                          const VkAllocationCallbacks* pAllocator);
 
-protected:
-    PFN_vkVoidFunction GetDeviceProcAddr_DispatchHelper(const char *pName);
-    PFN_vkVoidFunction GetInstanceProcAddr_DispatchHelper(const char *pName);
+ protected:
+  PFN_vkVoidFunction GetDeviceProcAddr_DispatchHelper(const char* pName);
+  PFN_vkVoidFunction GetInstanceProcAddr_DispatchHelper(const char* pName);
 
-    void PopulateInstanceLayerBypassDispatchTable();
-    void PopulateDeviceLayerBypassDispatchTable();
+  void PopulateInstanceLayerBypassDispatchTable();
+  void PopulateDeviceLayerBypassDispatchTable();
 
-private:
-    LayerBypassDispatch m_layerBypassDispatch = {};
+ private:
+  LayerBypassDispatch m_layerBypassDispatch = {};
 
-    VkInstance m_instance = VK_NULL_HANDLE;
-    VkDevice m_device = VK_NULL_HANDLE;
+  VkInstance m_instance = VK_NULL_HANDLE;
+  VkDevice m_device = VK_NULL_HANDLE;
 
-    VkPhysicalDeviceMemoryProperties m_physDevMemProps = {};
+  VkPhysicalDeviceMemoryProperties m_physDevMemProps = {};
 
-    // TODO: Replace with my own data structure in the FUTURE
-    std::unordered_map<VkDeviceMemory, uint32_t> m_allocToMemTypeMap;
-
+  // TODO: Replace with my own data structure in the FUTURE
+  std::unordered_map<VkDeviceMemory, uint32_t> m_allocToMemTypeMap;
 };
--- src/layerCore.h
+++ src/layerCore.h
@@ -2,11 +2,12 @@
 
 #include <vulkan/vk_layer.h>
 
-namespace GWD 
-{
+namespace GWD {
 
 // Helper functions to grab the next proc address in the dispatch chain
-PFN_vkVoidFunction GwdGetDispatchedDeviceProcAddr(VkDevice device, const char* pName);
-PFN_vkVoidFunction GwdGetDispatchedInstanceProcAddr(VkInstance instance, const char* pName);
+PFN_vkVoidFunction GwdGetDispatchedDeviceProcAddr(VkDevice device,
+                                                  const char* pName);
+PFN_vkVoidFunction GwdGetDispatchedInstanceProcAddr(VkInstance instance,
+                                                    const char* pName);
 
-}
+}  // namespace GWD
